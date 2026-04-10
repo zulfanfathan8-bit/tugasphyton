e577546fa28d9bf791879006048d808687716fa5
zulfanfathan8-bit <zulfanfathan8@gmail.com>
2026-04-10 13:36:23 +0700

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,25 @@
+# input jumlah hari
+x = int(input("Masukkan jumlah hari: "))
+
+# konversi
+tahun = x // 365
+sisa1 = x % 365
+
+bulan = sisa1 // 30
+hari = sisa1 % 30
+
+# output
+print("Hasil konversi:")
+print(tahun, "tahun,", bulan, "bulan,", hari, "hari") # input jumlah hari
+x = int(input("Masukkan jumlah hari: "))
+
+# konversi
+tahun = x // 365
+sisa1 = x % 365
+
+bulan = sisa1 // 30
+hari = sisa1 % 30
+
+# output
+print("Hasil konversi:")
+print(tahun, "tahun,", bulan, "bulan,", hari, "hari")
